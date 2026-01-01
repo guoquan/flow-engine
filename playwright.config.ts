f87d71021bac916fd0ea49da9194015f5d0ab0b0
Quan Guo <1118270+guoquan@users.noreply.github.com>
2026-01-01 23:00:57 +0800

    ci: fix baseURL casing in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html'], ['list']],
   use: {
-    baseUrl: 'http://127.0.0.1:4173',
+    baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
     screenshot: 'on',
   },

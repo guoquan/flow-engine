edff6abfcf5a9866df89fc74abdaa22ab0038311
Quan Guo <1118270+guoquan@users.noreply.github.com>
2026-01-05 13:27:20 +0800

    chore: update .gitignore and remove test artifacts from tracking

--- dist-e2e/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { FlowEngine } from './core/FlowEngine';
-export { AvatarLoader } from './core/AvatarLoader';
-export { AnimationController } from './core/AnimationController';
-export { BehaviorController } from './core/BehaviorController';
-export type { AvatarConfig, AnimationStateConfig, AvatarBehaviorState, BehaviorIntent } from './types';
-export { AvatarBehaviorStates } from './types';
